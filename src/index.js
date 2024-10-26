cfa2ff1b18ae7d3399f74631cfecd5fd550154f6
alinidi <alinareyz@gmail.com>
2024-10-26 20:29:22 +0500

    init: started\ended the task (oct 26, 2024, 8:30:05 PM)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const pair = {};
+  const stack = [];
+  const openBrackets = [];
+
+  for (const [open, close] of bracketsConfig) {
+    pair[open] = close;
+    openBrackets.push (open);
+  }
+  
+  for (let i = 0; i < str.length; i++){
+    const current = str[i];
+    if (openBrackets.includes(current)){
+      if (stack.length && stack [stack.length -1 ] === current){
+        stack.pop();
+      } else {
+        stack.push(current);
+      }
+    } else{
+      if (!stack.length || pair[stack.pop()] !== current) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
